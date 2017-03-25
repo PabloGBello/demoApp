0b9b2427efb2693a896c98fe239ab9e1ab82b4e0
Pato Suarez <77soft67@gmail.com>
2017-03-25 20:15:11 -0300

    Basic view with a button, click it and choose a image from the gallery and then upload it to imgur, finally get the imgur URL to the image

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,66 +3,18 @@
 angular.module('starter.controllers', [])
 
 
-.controller('MainCtrl', function($scope, $ionicPlatform, $cordovaImagePicker) {
-
-    $scope.imageURI = 'empty URI';  //es para tener la URI a mano
-
-    $ionicPlatform.ready(function() {
- 
-
-        $scope.selectImage = function() {
-       
-            var options = {
-                maximumImagesCount: 1, 
-                width: 800,
-                height: 800,
-                quality: 80           
-            };
-       
-            $cordovaImagePicker.getPictures(options).then(
-                function (results) {
-
-                    $scope.imageURI = results[0];
-
-                    convertToBase64(results[0],function(base64Image){
-                      alert(base64Image); 
-                    });
-                }, 
-                function(error) {
-                }
-            );
-  
-        }; 
-
-    });
-
-      
- 
-
+.controller('MainCtrl', function($scope, $ionicPlatform, Images) {
+    
+    $scope.selectImage = function() {
+        Images.uploadSelected();
+    }; 
+    
 });
 
-/**
- * Convierte la imagen dada por la URI a base64 
- *
- * @path string
- * @callback function se le pasa el contenido de la imagen como primer parametro
- */
-function convertToBase64(path,callback){
-    window.resolveLocalFileSystemURL(path, gotFile, fail);
-            
-    function fail(e) {
-          alert('File not found');
-    }
+       
 
-    function gotFile(fileEntry) {
-           fileEntry.file(function(file) {
-              var reader = new FileReader();
-              reader.onloadend = function(e) {
-                   var content = this.result;
-                   callback(content);
-              };
-              // Tmetodo readAsDatURL Method del file-plugin
-              reader.readAsDataURL(file);
-           });
-    }
-}
+
+
+
+
+
--- www/js/services.js
+++ www/js/services.js
@@ -1,51 +1,75 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Images', function($cordovaImagePicker, $http) {
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    function selectImage(){
+
+        // config para elegir las imagenes
+        var options = {
+              maximumImagesCount: 1, 
+              width: 800,
+              height: 800,
+              quality: 80           
+        };
+
+        // Abre el explorador y espera a que se elija una imagen
+        $cordovaImagePicker.getPictures(options)
+            .then(
+                function (results) {
+                  convertToBase64(results[0], uploadToImgur);
+            });
+    }
+
+    //Convierte la imagen dada por la URI a base64 y la retorna en el callback 
+    function convertToBase64(path, callback){
+        window.resolveLocalFileSystemURL(path, success, error);
+                
+        function success(fileEntry) {
+            fileEntry.file(function(file) {
+                var reader = new FileReader();
+                reader.onloadend = function(e) {
+                     var content = this.result;
+                     callback(content);
+                };
+                reader.readAsDataURL(file);
+            });
+        }
+
+        function error(error) {
+            alert("ERROR: file not found");
+        }
+    }
+
+    // la funcion que se va a ejecutar en el callback luego de la conversion a Base64
+    var uploadToImgur = function(content){
+
+        var settings = {
+              UPLOAD_URL: 'https://api.imgur.com/3/image',
+              UPLOAD_METHOD: 'POST',
+              API_KEY: 'Client-ID 26c2f281431807b'
+        };
+
+        var headerModel = { Authorization: settings.API_KEY },
+            dataModel   = { image: content.split(',')[1] };
+
+        $http({
+            method: settings.UPLOAD_METHOD,
+            url: settings.UPLOAD_URL,
+            headers: headerModel,
+            data:    dataModel
+        })
+        .then(
+            function successCallback(response) {
+                alert(response.data.data.link);
+            }, 
+            function errorCallback(response) {
+        });
+    };  
 
   return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
+      uploadSelected: function(){
+          return selectImage();
       }
-      return null;
-    }
   };
 });
 
