41be176b104dd5f0352d4177176afbe84fff0f75
Pato Suarez <77soft67@gmail.com>
2017-04-01 16:22:43 -0300

    UPDATE: con el Selector de imagenes se obtiene el base64 y se guarda. Luego con el base64 se sube a imgur y se guarda la URL resultante para utilizarla en el acople con el otro servicio. Se elimino el plugin ImagePicker. Se requiere el plugin CordovaCamera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,21 +5,36 @@ angular.module('starter.controllers', [])
 
 .controller('MainCtrl', function($scope, $ionicPlatform, Selector, Uploader) {
     
-    $scope.resultURI = '';
     $scope.uploadEnabled = false;
+    $scope.showEnabled = false;
+
+    var base64;
+    var imgurURL;
 
     $scope.selectImage = function() {
         Selector.selectImage()
         .then(function(value){
-            $scope.resultURI = value;
-            $scope.uploadEnabled = true;
+            base64 = value;
+            $scope.uploadEnabled = true; 
         })
         .catch(function(reason){
+
         });
     }; 
 
     $scope.uploadImage = function() {
-        Uploader.uploadToImgur($scope.resultURI);
+        Uploader.uploadToImgur(base64)
+        .then(function(value){
+            imgurURL = value;
+            $scope.showEnabled = true; 
+        })
+        .catch(function(reason){
+
+        });
+    };
+
+    $scope.showResult = function() {
+       alert(imgurURL);
     };
     
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,29 +1,27 @@
 angular.module('starter.services', [])
 
-.factory('Selector', function($cordovaImagePicker) {
+.factory('Selector', function($cordovaCamera) {
 
     function getURI(){
 
-        /*
-            Config para elegir las imagenes
-            No esta en constants
-        */
+        // Config para la el selector de imagenes
         var options = {
-              maximumImagesCount: 1, 
-              width: 800,
-              height: 800,
-              quality: 80           
+            destinationType : Camera.DestinationType.DATA_URL, 
+            sourceType : Camera.PictureSourceType.PHOTOLIBRARY, 
+            allowEdit : false,
+            saveToPhotoAlbum: false
         };
 
-        return $cordovaImagePicker.getPictures(options)
+        return $cordovaCamera.getPicture(options)
           .then(
-              function (results) {
-                return results[0];    
-              },
-              function(error){
-                return JSON.stringify(error);            
-          });       
+              function(imageData) {
+                return "data:image/jpeg;base64," + imageData;
+              }, 
+              function(error) {
+                return JSON.stringify(error);      
+          }); 
     }
+
 
   return {
       selectImage: function(){
@@ -34,52 +32,10 @@ angular.module('starter.services', [])
 
 .factory('Uploader', function($http, uploadSettings) {
 
-    function upload(URI){
-        convertToBase64(URI, getImgurLink);
-    }
-
-    /*
-    *  Convierte el archivo dado por la URI a base64 y posteriormente llama al callback registrado con el resultado 
-    */
-    function convertToBase64(path, callback){
-
-        /* 
-        *  Paso el path e intento traer el archivo
-        *  Success: el archivo (un objeto fileEntry)
-        */ 
-        window.resolveLocalFileSystemURL(path, success, error);
-                
-        function success(fileEntry) {
-
-            var readContent = function(file) {
-
-                var reader = new FileReader();
-
-                // El evento onloadend se va a ejecutar donde readAsDataURL se complete
-                reader.onloadend = function(e) {
-                     var content = this.result;
-                     callback(content);
-                };
-                reader.readAsDataURL(file);
-            };
-
-            fileEntry.file(readContent);
-        }
-
-        function error(error) {
-            callback("ERROR: file not found");
-        }
-    }
-
-    /*
-    *   Funcion registrada en el callback luego de la conversion a Base64
-    *   @content: resultado base64
-    *   No retorna, hace un alert: hay que adaptar con código de branch Conf_De_Busq
-    */
-    var getImgurLink = function(content){
+    function upload(base64){
 
         var headerModel = { Authorization: uploadSettings.apiKey },
-            dataModel   = { image: content.split(',')[1] };
+            dataModel   = { image: base64.split(',')[1] };
 
         return $http({
             method: uploadSettings.method,
@@ -89,17 +45,21 @@ angular.module('starter.services', [])
         })
         .then(
             function successCallback(response) {
-                alert(response.data.data.link);
+                return response.data.data.link;
             }, 
-            function errorCallback(response) {
-                alert(JSON.stringify(response));
+            function errorCallback(error) {
+                return JSON.stringify(error);
         });
-    };  
+    }
 
 
   return {
-      uploadToImgur: function(URI){
-          return upload(URI);
+      uploadToImgur: function(base64){
+          return upload(base64);
       }
   };
 });
+
+
+
+
